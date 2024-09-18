50d9e7c1802b856bfe60765f906fce30c92d1784
siva prasanth <150437046+Sivprasanth@users.noreply.github.com>
2024-09-19 01:56:51 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "siva-bucket-8096"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "mydynamodbtable"
     encrypt        = true
   }
   required_version = ">=0.13.0"
